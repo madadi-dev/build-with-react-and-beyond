177f594a37335d4fd8c61bed2fdd0a74c198b545
Jafar Madadi <jafar.madadi2026@gmail.com>
2026-05-19 14:13:53 +0430

    Creating a State Variable With useState

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const messages = [
     "Learn React ⚛️",
     "Apply for jobs 💼",
@@ -5,14 +7,14 @@ const messages = [
 ];
 
 export default function App() {
-    const step = 3;
+    const [step, setStep] = useState(1);
 
     function handlePrevious() {
-        alert('Previous');
+        if (step > 1) setStep(step - 1);
     }
 
     function handleNext() {
-        alert('Next');
+        if (step < 3) setStep(step + 1);
     }
 
     return <div className="steps">
